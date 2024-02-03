fd1e6a69f3aaf8bfbe3f7bea54225eaacb2f8ea1
iamminseongKim <minseongkimim@gmail.com>
2024-02-03 14:06:46 +0900

    컨트롤러 테스트 코드 작성

--- src/test/java/kbo/whoareya/player/controller/PlayerControllerTest.java
+++ src/test/java/kbo/whoareya/player/controller/PlayerControllerTest.java
@@ -1,6 +1,5 @@
 package kbo.whoareya.player.controller;
 
-import com.jayway.jsonpath.JsonPath;
 import kbo.whoareya.player.dto.CreatePlayerDto;
 import kbo.whoareya.player.dto.PlayerInfoDto;
 import kbo.whoareya.player.dto.UserPlayerResultDto;
@@ -13,24 +12,15 @@ import kbo.whoareya.team.entity.Team;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.transaction.annotation.Transactional;
+
 
 import java.time.LocalDate;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
